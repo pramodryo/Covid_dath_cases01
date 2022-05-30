abfb7d17eff02278c38527d08b6791b2ae5990e1
Analyst Pramod <105561410+pramodryo@users.noreply.github.com>
2022-05-30 12:04:31 +0530

    Update SQLite_Covid_Death_Cases.sql

--- SQLite_Covid_Death_Cases.sql
+++ SQLite_Covid_Death_Cases.sql
@@ -6,20 +6,22 @@
 --SELECT * FROM C19INDIA;
 
 -- find the total active cases
---SELECT sum (active_cases) from C19INDIA;
+SELECT sum (active_cases) from C19INDIA;
 
 
 -- find the top five state which has highest active cases
---SELECT "state/ut", active_cases FROM C19INDIA ORDER by active_cases DESC limit 5;
+SELECT "state/ut", active_cases FROM C19INDIA ORDER by active_cases DESC limit 5;
 
 --create new table as high_death and add top 10 state which has maximum death.
---CREATE TABLE High_death as  SELECT "state/ut", active_cases , death FROM C19INDIA ORDER by death DESC limit 10;
+CREATE TABLE High_death as  SELECT "state/ut", active_cases , death 
+FROM C19INDIA ORDER by death DESC limit 10;
+
 --SELECT * FROM High_death;
 
 
 -- find the state which has maximum recovery percentage to total cases. 
---SELECT "state/ut",confirmed_cases, "cured/discharged", ("cured/discharged" * 100 / confirmed_cases) 
---as recovry_rate FROM C19INDIA ORDER by recovry_rate DESC;
+SELECT "state/ut",confirmed_cases, "cured/discharged", ("cured/discharged" * 100 / confirmed_cases) 
+as recovry_rate FROM C19INDIA ORDER by recovry_rate DESC;
 
 
 
@@ -34,5 +36,6 @@ as India_death_percentage from C19INDIA;
 
 
 -- find the state which has lees than 5 active cases and store it in table  
---CREATE table Less_active_cass as select "state/ut", active_cases FROM C19INDIA where active_cases <= 5;
---SELECT * from Less_active_cass;
+CREATE table Less_active_cass as select "state/ut", active_cases 
+FROM C19INDIA where active_cases <= 5;
+SELECT * from Less_active_cass;
